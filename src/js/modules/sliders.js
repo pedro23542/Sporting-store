a41fdd98a3e3ba7b6236605440239e5d5e667b50
pedro23542 <arsensankin@yandex.ru>
2021-07-17 19:44:25 +0500

    finished layout of index page

--- src/js/modules/sliders.js
+++ src/js/modules/sliders.js
@@ -1,11 +1,47 @@
 import Swiper from 'swiper/bundle';
 
 const sliders = () => {
-  const mainSlider = new Swiper('.swiper-container', {
+  const mainSlider = new Swiper('.main-slider__container', {
     slidesPerView: 1,
     spaceBetween: 0,
     centeredSlides: true,
   });
+
+  //mainSlider pagination logic
+  const slides = document.querySelectorAll('.main-slider__slide');
+  const paginationBody = document.querySelector('.main-slider__pagination');
+  
+  slides.forEach((elem, index) => {
+    paginationBody.insertAdjacentHTML(
+      'beforeend', 
+      `<div class="main-slider__pagination-item" data-index="${index}">${index + 1}</div>`
+      );
+  });
+
+  const bulets =  document.querySelectorAll('.main-slider__pagination-item');
+  bulets[0].classList.add('main-slider__pagination--active');
+
+  bulets.forEach((elem, index) => {
+    elem.style.backgroundImage = `url(images/content/slide${index+1}.jpg)`;
+    elem.addEventListener('click', () => {
+      
+    });
+  });
+
+
+
+  const brandsSlider = new Swiper('.brands-labels__container', {
+    slidesPerView: 5,
+    centeredSlides: true,
+    centeredSlidesBounds: true,
+    navigation: {
+      nextEl: '.brands-labels__button-prev',
+      prevEl: '.brands-labels__button-next',
+    },
+  });
+
+
+
 };
 
 export default sliders;
